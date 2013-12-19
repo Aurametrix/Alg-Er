7c68748bc11d5a16272f1efef039b0859f587de0
Irene <irene@aurametrix.com>
2013-12-19 11:44:09 -0800

    multithreaded version

--- SORT/mergesort.erl
+++ SORT/mergesort.erl
@@ -1,4 +1,13 @@
-mergeSort(L) when length(L) == 1 -> L;
-mergeSort(L) when length(L) > 1 ->
+pMergeSort(L) when length(L) == 1 -> L;
+pMergeSort(L) when length(L) > 1 ->
     {L1, L2} = lists:split(length(L) div 2, L),
-    lists:merge(mergeSort(L1), mergeSort(L2)).
+    spawn(mergesort, pMergeSort2, [L1, self()]),
+    spawn(mergesort, pMergeSort2, [L2, self()]),
+    mergeResults([]).
+ 
+pMergeSort2(L, Parent) when length(L) == 1 -> Parent ! L;
+pMergeSort2(L, Parent) when length(L) > 1 ->
+    {L1, L2} = lists:split(length(L) div 2, L),
+    spawn(mergesort, pMergeSort2, [L1, self()]),
+    spawn(mergesort, pMergeSort2, [L2, self()]),
+    Parent ! mergeResults([]).
