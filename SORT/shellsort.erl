7f5fa5bed18c5e32cd4f02c997465d0a4aa21a73
Irene <irene@aurametrix.com>
2014-03-07 17:46:07 -0800

    multi-threaded shellsort

--- SORT/shellsort.erl
+++ SORT/shellsort.erl
@@ -1,8 +1,17 @@
 %% R.C.
 %%
-%% Simple, one process version
+%% multi-process version spawns a new process each time it splits
 %% Takes advantage of built-in lists:split and lists:merge functions
 mergeSort(L) when length(L) == 1 -> L;
 mergeSort(L) when length(L) > 1 ->
     {L1, L2} = lists:split(length(L) div 2, L),
-    lists:merge(mergeSort(L1), mergeSort(L2)).
+    spawn(mergesort, pMergeSort2, [L1, self()]),
+    spawn(mergesort, pMergeSort2, [L2, self()]),
+    mergeResults([]).
+ 
+pMergeSort2(L, Parent) when length(L) == 1 -> Parent ! L;
+pMergeSort2(L, Parent) when length(L) > 1 ->
+    {L1, L2} = lists:split(length(L) div 2, L),
+    spawn(mergesort, pMergeSort2, [L1, self()]),
+    spawn(mergesort, pMergeSort2, [L2, self()]),
+    Parent ! mergeResults([]).
