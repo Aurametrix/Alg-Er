3b1b25315407cf1c9cc9fe7df95bc9a7c676162a
Aurametrix <irene@aurametrix.com>
2015-10-09 07:21:37 -0700

    optimized

--- Problems/100doors.erl
+++ Problems/100doors.erl
@@ -1,16 +1,5 @@
--module(hundoors).
- 
--export([go/0]).
- 
-toggle(closed) -> open;
-toggle(open) -> closed.
- 
-go() -> go([closed || _ <- lists:seq(1, 100)],[], 1, 1).
-go([], L, N, _I) when N =:= 101 -> lists:reverse(L);
-go([], L, N, _I) -> go(lists:reverse(L), [], N + 1, 1);
-go([H|T], L, N, I) ->
-  H2 = case I rem N of
-    0 -> toggle(H);
-    _ -> H
-  end,
-  go(T, [H2|L], N, I + 1).
+doors() ->
+     F = fun(X) -> Root = math:pow(X,0.5), Root == trunc(Root) end,
+     Out = fun(X, true) -> io:format("Door ~p: open~n",[X]);
+              (X, false)-> io:format("Door ~p: close~n",[X]) end,
+     [Out(X,F(X)) || X <- lists:seq(1,100)].
