c919d06b6a70c904af9d1a29936dffaa95c9525a
Aurametrix <irene@aurametrix.com>
2017-08-21 21:14:19 -0500

    let rule
    https://github.com/fenollp/erlscp
    a supercompiler pass for Erlang: 
    https://news.ycombinator.com/item?id=15064127
     https://github.com/weinholt

---
+++ Problems/cmp.erl
@@ -0,0 +1,15 @@
+R7/R8/R9
+  let!!!
+   Lhs='State@7' Rhs={tuple,7,[{atom,7,some},{string,7,"state"}]}
+   Body={match,8,
+               {var,8,'Fs@527'},
+               {cons,8,
+                     {'fun',8,{function,two,2}},
+                     {cons,8,
+                           {'fun',9,{function,three,2}},
+                           {cons,8,
+                                 {'fun',10,{function,five,2}},
+                                 {cons,8,
+                                       {'fun',11,{function,'TWO',2}},
+                                      {nil,8}}}}}}
+  no residual let.
